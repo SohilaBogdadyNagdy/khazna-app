98889fa2bf6e92437ccd77f0de646f53961fcdbb
sohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-07-11 00:29:12 +0200

    Products model

--- products-service/index.js
+++ products-service/index.js
@@ -1,22 +1,15 @@
 
 var express = require('express');
-const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 const host = '0.0.0.0';
 const port = 80;
-const db_location = process.env.DB_LOCATION;
-const db_user = process.env.DB_USER;
-const db_password = process.env.DB_PASSWORD;
-const db_name  = process.env.DB_NAME;
-const auth_service_url = process.env.AUTH_URL;
- 
- 
+
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Products');
 });
 
 app.listen(port, host);
---
+++ products-service/model/index.js
@@ -0,0 +1,38 @@
+const db = require("../db");
+
+!async function createTable() {
+    const tableQuery = `CREATE TABLE IF NOT EXISTS products (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        brand VARCHAR(255) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        price DECIMAL(8,2) NOT NULL,
+        state VARCHAR(255),
+        )`;
+   
+    await db.query(tableQuery);    
+}();
+
+exports.findAll = async function () {
+    const results = await db.query("SELECT * FROM products");
+    return results[0];
+}
+
+exports.findOne = async function (id) {
+    const result = await db.query("SELECT * FROM products WHERE id=?", id);
+    return result[0];
+}
+
+exports.findOneByName = async function (name) {
+    const result = await db.query("SELECT * FROM products WHERE name=?", name);
+    return result[0];
+}
+
+exports.create = async function (name, brand, category, price) {
+    await db.query("INSERT INTO products(name, brand, category, price) VALUES (?, ?, ?, ?)", [name, brand, category, price]);
+}
+
+exports.updateState = async function (id, state) {
+    await db.query("UPDATE products SET state=? WHERE id=?",
+        [state, id]);
+}
