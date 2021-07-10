ac5840d7872a8aba0e7a99902910ef30992be874
sohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-07-10 21:28:59 +0200

    fix run product and auth service

--- auth-service/index.js
+++ auth-service/index.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 const port = 80;
+const host = '0.0.0.0';
 const db_location = process.env.DB_LOCATION;
 const db_user = process.env.DB_USER;
 const db_password = process.env.DB_PASSWORD;
@@ -13,3 +14,10 @@ const db_name  = process.env.DB_NAME;
 app.use(express.json());
 app.use(cors());
  
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, host);
+console.log(`Running on http://${host}:${port}`);
--- products-service/index.js
+++ products-service/index.js
@@ -3,13 +3,21 @@ var express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
-const port = 81;
+const host = '0.0.0.0';
+const port = 80;
 const db_location = process.env.DB_LOCATION;
 const db_user = process.env.DB_USER;
 const db_password = process.env.DB_PASSWORD;
 const db_name  = process.env.DB_NAME;
+const auth_service_url = process.env.AUTH_URL;
  
  
 app.use(express.json());
 app.use(cors());
- 
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, host);
+console.log(`Running on http://${host}:${port}`);
