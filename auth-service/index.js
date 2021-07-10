2dc05c36633de3ca37c2ddbf48cb147f8970372a
sohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-07-10 15:54:00 +0200

    Init app

---
+++ auth-service/index.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const app = express();
+const port = 80;
+const db_location = process.env.DB_LOCATION;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+const db_name  = process.env.DB_NAME;
+ 
+ 
+app.use(express.json());
+app.use(cors());
+ 
