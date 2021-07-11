05758e1dabbc343b14a0ad6b19300b57366b4f6d
sohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-07-11 03:08:04 +0200

    Update auth middleware

--- products-service/middleware/auth.js
+++ products-service/middleware/auth.js
@@ -1,19 +1,21 @@
 const jwt = require('jsonwebtoken');
-const secret = 'secret%&(!';
 
 module.exports = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+     return res.status(401).json({ error: 'token missing' })
+  }
   try {
-    const token = req.headers.authorization.split(' ')[1] || '';
-    const decodedToken = await jwt.verify(token, secret);
-    const userId = decodedToken.userId;
-    if (userId) {
-      req.user = user;
-      next();
-    }
-  } catch(exp) {
-    console.log(exp.message);
-    return res.status(401).json({
-      message: 'Not Authorized'
-    });
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    return res.status(400).json({ error: 'token invalid' })
   }
 };
