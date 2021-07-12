49c560a40e27c483a3d0b36e063d507ebda19a02
sohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-07-13 01:47:56 +0200

    Add validation to request and cancel order api

--- products-service/controller/index.js
+++ products-service/controller/index.js
@@ -1,21 +1,24 @@
 const productsModel = require("../model/index");
 
-exports.listProducts = (req, res, next) => {
+exports.listProducts = async (req, res, next) => {
     try {
         const balance = req.user.balance;
-        const data = productsModel.findByUserBalance(balance);
+        const data = await productsModel.findByUserBalance(balance);
         return res.status(200).json({data});
     } catch(exp) {
         return res.status(500).json({message: exp.message});
     }
 };
 
-exports.request = (req, res, next) => {
+exports.request = async (req, res, next) => {
     try {
         console.log('request product')
         const user = req.user;
         const { id } = req.params;
-        product = productsModel.findOne(id)
+        product = await productsModel.findOne(id)
+        if (!product) {
+            return res.status(400).json({ message: 'Product not found.'}) 
+        }
         productsModel.updateState(id, 'REQUESTED')
         // deductFromUserBalance(product.price)
         return res.status(200).json({message: 'Done Successfully'});
@@ -24,13 +27,16 @@ exports.request = (req, res, next) => {
     }
 };
 
-exports.cancel = (req, res, next) => {
+exports.cancel = async (req, res, next) => {
     try {
         const user = req.user;
         const { id } = req.params;
-        product = productsModel.findOne(id)
+        product = await productsModel.findOne(id);
+        if (!product) {
+            return res.status(400).json({ message: 'Product not found.'}) 
+        }
         if (product.state == 'Delivered') {
-            res.state(400).json({ message: 'Not Allowd to cancel product in delivered state.'})
+            return res.status(400).json({ message: 'Not Allowd to cancel product in delivered state.'})
         }
         productsModel.updateState(id, 'AVAILABE')
         // addToUserBalance(product.price)
